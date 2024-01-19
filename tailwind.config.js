68afd8a12bbdf870843e5028ec5d47547919fac8
parthpatel <parthpatel5333@gamil.com>
2024-01-19 17:21:55 -0500

    updated node version and remover unnecessary files

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["/Users/sambhu/Desktop/my-portfolio/public/index.html"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
